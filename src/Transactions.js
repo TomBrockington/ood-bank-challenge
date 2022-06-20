826a25dfc32ec7aeb8c1dbac56693c911674fb07
xtombrock <xtombrock1989@gmail.com>
2022-06-20 13:00:23 +0100

    safety

---
+++ src/Transactions.js
@@ -0,0 +1,14 @@
+class Transactions {
+    constructor() {
+
+    }
+
+    // take info
+    // return transaction object 
+    this.transactions.push(
+        date:
+        credit:
+        debit:
+        balance: 
+    )
+}
